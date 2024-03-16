b5783294ea5589330664eb4b46474ce21f8c2258
TNZ <tnz@free.fr>
2024-03-16 09:26:45 +0100

    Version 9.1 RC Watch List : update date format

--- beta/Watch-Refresh.sh
+++ beta/Watch-Refresh.sh
@@ -22,7 +22,7 @@ while [ -d /tmp ]
 do
     $* > $WATCH_TMP 2>&1
     clear
-    printf "\033[37;44m *** $(date +'%Y %m %d - %Hh %Mm %Ss') *** \033[m\n"
+    printf "\033[37;44m *** $(date +'%Y/%m/%d - %Hh %Mm %Ss') *** \033[m\n"
     cat   $WATCH_TMP
     rm -f $WATCH_TMP
     sleep 10
