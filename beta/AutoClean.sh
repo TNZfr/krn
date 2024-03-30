ea01c0b2d434d384f8d68409cbf6acdd45e249f3
TNZfr <tnz@free.fr>
2024-03-30 22:55:01 +0100

    Version 9.1 Beta / RC krn autoclean : initial krn autoremove : initial

---
+++ beta/AutoClean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+. $KRN_EXE/_libkernel.sh
+
+#-------------------------------------------------------------------------------
+# Main
+#
+
+_RefreshWorkspaceList
+WorkspaceList=$KRN_WORKSPACE/.CompletionList
+
+NbItem=$(grep -v ",cfg," $WorkspaceList|wc -l)
+if [ $NbItem -eq 0 ]
+then
+    echo ""
+    echo "Workspace install already clean."
+    echo ""
+    exit 0
+fi
+
+ckc_list=$(  grep    ",ckc," $WorkspaceList                |cut -d',' -f4)
+other_list=$(grep -v ",ckc," $WorkspaceList|grep -v ",cfg,"|cut -d',' -f1)
+
+Purge.sh $ckc_list $other_list
+
+exit 0
