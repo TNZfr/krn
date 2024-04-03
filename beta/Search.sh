4a4fdc1e346442bcaf1a6821b25f7a6895e2e0c9
TNZfr <tnz@free.fr>
2024-04-03 16:30:47 +0200

    Version 9.1 RC4 Search : update for build directories

--- beta/Search.sh
+++ beta/Search.sh
@@ -27,12 +27,13 @@ mkdir -p $TempDir
 
 echo ""
 echo "Availability : "
-echo -e "\t\033[32mUbuntu\033[m ................... : Debian Package from PPA Kernel Ubuntu"
-echo -e "\t\033[32mWorkspace deb package\033[m .... : Local Debian package"
-echo -e "\t\033[32mWorkspace rpm package\033[m .... : Local Redhat package"
-echo -e "\t\033[mWorkspace compil directory\033[m : Local build directory (Arch / Gentoo)"
-echo -e "\tGit / Cdn ................ : Sources from kernel.org for compilation"
-echo -e "\t\033[35mckc-\033[3mVersion-Label\033[m ........ : Local custom kernel packages"
+echo -e "\t\033[32mUbuntu\033[m .............. : Debian Package from PPA Kernel Ubuntu"
+echo -e "\t\033[32mWorkspace deb package\033[m : Local Debian package"
+echo -e "\t\033[32mWorkspace rpm package\033[m : Local Redhat package"
+echo -e "\t\033[mWorkspace directory\033[m   : Local directory (Arch / Gentoo)"
+echo -e "\tGit / Cdn ........... : Sources from kernel.org for compilation"
+echo -e "\t\033[35mckc-\033[3mVersion-Label\033[m ... : Local custom kernel packages"
+echo -e "\t\033[33mWorkspace build\033[m ..... : Local build directory"
 echo ""
 echo -e "Database update :\033[34m" $(stat $RemoteVersion|grep ^Modify|cut -c9-27) "\033[m(krn Update to refresh database)"
 echo ""
@@ -63,7 +64,7 @@ do
 		    ckc) echo -e "[\033[35m$(echo $Record|cut -d',' -f4)\033[m]" >> $TempDir/$Version ;;
 		    deb) echo -e "[\033[32mWorkspace deb package\033[m]"         >> $TempDir/$Version ;;
 		    rpm) echo -e "[\033[32mWorkspace rpm package\033[m]"         >> $TempDir/$Version ;;
-		    arc) echo -e "[\033[mWorkspace compil directory\033[m]"      >> $TempDir/$Version ;;
+		    arc) echo -e "[\033[mWorkspace directory\033[m]"             >> $TempDir/$Version ;;
 		    dir) echo -e "[\033[33mWorkspace build\033[m]"               >> $TempDir/$Version ;;
 		esac
 	    done
