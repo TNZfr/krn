757372e8f50cb0d7792d274d2e251c66e03baac1
TNZ <tnz@free.fr>
2024-10-01 18:45:41 +0200

    Version 10.5 * Curses : new elapsed time algorythm (use of integer instead of float/double) * Curses : now display milli-seconds AND micro-seconds

--- last/src/Curses-elapsed.c
+++ last/src/Curses-elapsed.c
@@ -6,13 +6,35 @@
 
 #include "Curses.h"
 
+typedef struct
+{
+  long Second;
+  long Micro;
+} SECMIC;
+
+//------------------------------------------------------------------------------
+void ParseSecMic (SECMIC *SecMic, char *String)
+{
+  char *Point;
+
+  if (!String || !SecMic) return;
+  Point=strchr(String,'.');
+
+  *Point='\0';
+  SecMic->Second = atoi (String);
+
+  *Point   = '.';
+  Point[7] = '\0';
+  SecMic->Micro = atoi (&Point[1]);
+}
+
 //------------------------------------------------------------------------------
 void DSP_Elapsed (CELL_ELAPSED *Cell)
 {
-  double ValDebut, ValFin, Delta;
+  SECMIC ValDebut, ValFin, Delta;
   char  *Debut, *Fin;
 
-  int  NbJour,NbHeure,NbMinute,NbSeconde,NbMilli;
+  int  NbJour,NbHeure,NbMinute,NbSeconde,NbMilli,NbMicro;
   char Buffer[32];
 
   // Terminated ?
@@ -23,10 +45,11 @@ void DSP_Elapsed (CELL_ELAPSED *Cell)
   Fin   = getenv (Cell->Fin);
   if (!Debut) return;
 
-  ValDebut = atof(Debut);
+  ParseSecMic (&ValDebut, Debut);
+
   if (Fin)
   {
-    ValFin = atof(Fin);
+    ParseSecMic (&ValFin, Fin);
     Cell->Completed = 1;
   }
   else
@@ -34,23 +57,34 @@ void DSP_Elapsed (CELL_ELAPSED *Cell)
     struct timeval Now; //Now.tv_sec et Now.tv_usec
 
     gettimeofday (&Now, NULL);
-    ValFin = (double) (Now.tv_sec + Now.tv_usec / 1000000);    
+    ValFin.Second = (long) Now.tv_sec;
+    ValFin.Micro  = (long) Now.tv_usec;
   }
 
-  Delta = ValFin - ValDebut;
-  if (Delta < 0.0) Delta = 0.0;
-  NbSeconde = (int) Delta;
-  NbMilli   = (int) ((Delta - (double) NbSeconde) * 1000.0);
+  Delta.Second = ValFin.Second - ValDebut.Second;
+  Delta.Micro  = ValFin.Micro  - ValDebut.Micro;
+
+  if (Delta.Micro < 0)
+    {
+      Delta.Second -= 1;
+      Delta.Micro  += 1000000;
+    }
+  
+  NbMicro   = Delta.Micro % 1000;
+  NbMilli   = Delta.Micro / 1000;
+
+  NbSeconde = (int) Delta.Second;
   NbMinute  = NbSeconde / 60; NbSeconde %= 60;
   NbHeure   = NbMinute  / 60; NbMinute  %= 60;
   NbJour    = NbHeure   / 24; NbHeure   %= 24;
 
-  if      (NbJour)   sprintf (Buffer,"%dd %dh %dm %ds.%03d ", NbJour,NbHeure,NbMinute,NbSeconde,NbMilli);
-  else if (NbHeure)  sprintf (Buffer,"%dh %dm %ds.%03d ", NbHeure,NbMinute,NbSeconde,NbMilli);
-  else if (NbMinute) sprintf (Buffer,"%dm %ds.%03d ", NbMinute,NbSeconde,NbMilli);
-  else               sprintf (Buffer,"%ds.%03d ", NbSeconde,NbMilli);
+  if      (NbJour)   sprintf (Buffer,"%dd %dh %dm %ds.%03d", NbJour,NbHeure,NbMinute,NbSeconde,NbMilli);
+  else if (NbHeure)  sprintf (Buffer,    "%dh %dm %ds.%03d",        NbHeure,NbMinute,NbSeconde,NbMilli);
+  else if (NbMinute) sprintf (Buffer,        "%dm %ds.%03d",                NbMinute,NbSeconde,NbMilli);
+  else               sprintf (Buffer,            "%ds.%03d",                         NbSeconde,NbMilli);
   
   if (Fin) strcpy  (Cell->Buffer, Buffer);
-  else     sprintf (Cell->Buffer,"%c[22;34m%s%c[m",27,Buffer,27);
-}
+  else     sprintf (Cell->Buffer, "%c[22;1;34m%s", 27,Buffer);
 
+  sprintf (&Cell->Buffer[strlen(Cell->Buffer)]," %c[22;3;36m%03d%c[m ",27,NbMicro,27);
+}
